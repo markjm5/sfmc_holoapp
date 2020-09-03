59af8302ba82b0c7aa96d2a7ace8b56c2e69f74e
Mark Mukherjee <65020036+markjm5@users.noreply.github.com>
2020-09-03 21:57:44 +0800

    working: config to install firebase, creating a new firebase project and updating GeneratedPluginRegistrant file to fix error

--- android/app/src/main/kotlin/com/example/sfmc_holoapp/MainActivity.java
+++ android/app/src/main/kotlin/com/example/sfmc_holoapp/MainActivity.java
@@ -71,8 +71,7 @@ public class MainActivity extends FlutterActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        //GeneratedPluginRegistrant.registerWith(this);
-
+        GeneratedPluginRegistrant.registerWith(this);
 
         new MethodChannel(getFlutterView(), CHANNEL).setMethodCallHandler(new MethodChannel.MethodCallHandler(){
             @Override
